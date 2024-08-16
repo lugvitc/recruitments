aeab5c43744a53ba300a1c372325298f1d8626b0
WizzyGeek <ojasscoding@gmail.com>
2024-08-17 00:35:31 +0530

    add: ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,44 @@ import {
   createRoutesFromElements,
   Route,
   RouterProvider,
+  useRouteError,
 } from "react-router-dom";
 
 import Root from "./pages";
 import NavLayout from "./components/NavLayout";
 import Login from "./pages/login";
-import Apply from "./pages/apply";
+// import Apply from "./pages/apply";
 import { useEffect, useState } from "react";
 import { supabase, SessionContext } from "./supabase";
-// import Book from "./pages/book"
+import Book from "./pages/book"
 import Level from "./pages/level";
 // import Result from "./pages/results"
+
+function ErrorBoundary() {
+  const error: any = useRouteError();
+  console.log(error);
+
+  return (
+    <div className="flex justify-center items-center h-full w-full">
+      <div className="max-w-80">
+        <h1 className="text-7xl text-yellow-400 uppercase font-mono font-semibold italic">{error.status}</h1>
+        <p className="text-3xl">
+          {error.statusText}
+        </p>
+        <p className="font-mono">{error.data}</p>
+      </div>
+    </div>
+  )
+}
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route element={<NavLayout />}>
+      <Route element={<NavLayout />} errorElement={<ErrorBoundary />}>
         <Route path="/" element={<Root />} />
         <Route path="auth" element={<Login />} />
-        <Route path="apply" element={<Apply />} />
-        {/* <Route path="book" element={<Book />} /> */}
+        {/* <Route path="apply" element={<Apply />} /> */}
+        <Route path="book" element={<Book />} />
         <Route path="admin" element={<Level />} />
         {/* <Route path="result" element={<Result />} /> */}
       </Route>
