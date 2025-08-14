9436634a6986f73c6b34b9fe86fc3177ad8f27f4
celestix <i@anodite.me>
2025-08-14 23:54:14 +0530

    fix supabase url

--- src/supabase.tsx
+++ src/supabase.tsx
@@ -2,8 +2,8 @@ import { createClient, Session } from '@supabase/supabase-js'
 import { createContext } from 'react'
 import { Database } from './supabase.types'
 
-export const supabaseUrl = 'https://twvyrbcvrsbsaxjymrcr.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3dnlyYmN2cnNic2F4anltcmNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc4NTQwMDUsImV4cCI6MjAzMzQzMDAwNX0.PE2jnWmWTeNvwQQSDXrI3ERPkrIVYVIBoR5nqopZefY"
+export const supabaseUrl = 'https://iwojatlubezorinlrxkg.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3b2phdGx1YmV6b3JpbmxyeGtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxODg1MzUsImV4cCI6MjA3MDc2NDUzNX0.neppMFelpXSCdVd2k7hOl8OI5MoJa1FJEEY0_BxxZtk"
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey)
 
 export const SessionContext = createContext<Session | null>(null);
