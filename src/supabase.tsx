4684c75d3fcf2b4c3f7de98102c3182c5cea4f85
fexa daddy <43469241+Fexaop@users.noreply.github.com>
2026-07-20 10:52:13 +0530

    Update supabase.tsx

--- src/supabase.tsx
+++ src/supabase.tsx
@@ -2,8 +2,8 @@ import { createClient, Session } from '@supabase/supabase-js'
 import { createContext } from 'react'
 import { Database } from './supabase.types'
 
-export const supabaseUrl = 'https://twvyrbcvrsbsaxjymrcr.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3dnlyYmN2cnNic2F4anltcmNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc4NTQwMDUsImV4cCI6MjAzMzQzMDAwNX0.PE2jnWmWTeNvwQQSDXrI3ERPkrIVYVIBoR5nqopZefY"
+export const supabaseUrl = 'https://awkxnthlsisqjotalipj.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3a3hudGhsc2lzcWpvdGFsaXBqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1MTYyNzMsImV4cCI6MjEwMDA5MjI3M30.1zTlEgvtGya_KXYrnAO3wHmC8y6XJ5-aX3DOMn_WqNw"
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey)
 
 export const SessionContext = createContext<Session | null>(null);
